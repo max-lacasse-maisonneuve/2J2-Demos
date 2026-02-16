9d6dbf91c172bf58fe17bc3960a622aae94a3321
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2026-02-16 10:32:02 -0500

    Ajout cours 8

---
+++ Cours08_detectionTouches/Assets/Scripts/DeplacementVoiture.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class DeplacementVoiture : MonoBehaviour
+{
+
+
+    public float vitesseDeplacement = 7.5f;
+    public float vitesseRotation = 150;
+
+    void OnEnable()
+    {
+
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
