b7d7ba01def2334cb98180c0bf8e834d24943f7e
Maxime <mlacassegermain@cmaisonneuve.qc.ca>
2026-02-16 09:24:34 -0500

    Ajout cours 7

---
+++ Cours07_GameObjects/Assets/Scripts/missile/MissileTeteChercheuse.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MissileTeteChercheuse : MonoBehaviour
+{
+    // Variables publiques
+
+
+    // Variables privées
+
+
+    void Start()
+    {
+        // On trouve les références
+
+    }
+
+    void Update()
+    {
+
+    }
+}
